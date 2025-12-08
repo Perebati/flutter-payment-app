377cf898d6808a44b6ab3650f16ec8729943e69b
Lucas Batista Pereira <lucas.bpereira1999@gmail.com>
2025-12-08 15:59:06 -0300

    CMAKE adjustments

--- flutter_payment_app/lib/rust_gateway.dart
+++ flutter_payment_app/lib/rust_gateway.dart
@@ -1,7 +1,34 @@
-import 'dart:ffi';
+import 'dart:ffi' as ffi;
 import 'dart:io';
+import 'package:ffi/ffi.dart' as pkg_ffi;
 
-import 'package:ffi/ffi.dart';
+/// Rust struct translated to an FFI-compatible Dart representation.
+final class FfiPaymentResult extends ffi.Struct {
+  @ffi.Int32()
+  external int status;
+
+  @ffi.Double()
+  external double riskScore;
+
+  external ffi.Pointer<pkg_ffi.Utf8> message;
+}
+
+typedef _ProcessPaymentNative = FfiPaymentResult Function(
+  ffi.Double amount,
+  ffi.Double tip,
+  ffi.Int32 method,
+);
+typedef _ProcessPaymentDart = FfiPaymentResult Function(
+  double amount,
+  double tip,
+  int method,
+);
+
+typedef _FreeStringNative = ffi.Void Function(ffi.Pointer<ffi.Char>);
+typedef _FreeStringDart = void Function(ffi.Pointer<ffi.Char>);
+
+typedef _DescribeMethodNative = ffi.Pointer<ffi.Char> Function(ffi.Int32 method);
+typedef _DescribeMethodDart = ffi.Pointer<ffi.Char> Function(int method);
 
 /// Wrapper around the Rust dynamic library used by this example.
 class RustPaymentGateway {
@@ -10,9 +37,8 @@ class RustPaymentGateway {
       _library = _openLibrary();
       _processPayment = _library.lookupFunction<_ProcessPaymentNative,
           _ProcessPaymentDart>('process_payment');
-      _freeRustString =
-          _library.lookupFunction<_FreeStringNative, _FreeStringDart>(
-              'free_rust_string');
+      _freeRustString = _library.lookupFunction<_FreeStringNative,
+          _FreeStringDart>('free_rust_string');
       _describeMethod = _library.lookupFunction<_DescribeMethodNative,
           _DescribeMethodDart>('describe_method');
       _initialized = true;
@@ -22,7 +48,7 @@ class RustPaymentGateway {
     }
   }
 
-  late final DynamicLibrary _library;
+  late final ffi.DynamicLibrary _library;
   late final _ProcessPaymentDart _processPayment;
   late final _FreeStringDart _freeRustString;
   late final _DescribeMethodDart _describeMethod;
@@ -49,7 +75,7 @@ class RustPaymentGateway {
 
     final FfiPaymentResult result = _processPayment(amount, tip, methodIndex);
     final String message = result.message.toDartString();
-    _freeRustString(result.message);
+    _freeRustString(result.message.cast<ffi.Char>());
 
     final String methodDescription = _describeAndFreeMethod(methodIndex);
 
@@ -61,25 +87,28 @@ class RustPaymentGateway {
     );
   }
 
-  DynamicLibrary _openLibrary() {
+  ffi.DynamicLibrary _openLibrary() {
     if (Platform.isAndroid || Platform.isLinux) {
-      return DynamicLibrary.open('librust_payment_engine.so');
+      return ffi.DynamicLibrary.open('librust_payment_engine.so');
     }
     if (Platform.isWindows) {
-      return DynamicLibrary.open('rust_payment_engine.dll');
+      return ffi.DynamicLibrary.open('rust_payment_engine.dll');
     }
     if (Platform.isMacOS || Platform.isIOS) {
-      return DynamicLibrary.open('librust_payment_engine.dylib');
+      return ffi.DynamicLibrary.open('librust_payment_engine.dylib');
     }
 
     throw UnsupportedError('Plataforma não suportada para o FFI com Rust');
   }
 
   String _describeAndFreeMethod(int methodIndex) {
-    final Pointer<Utf8> pointer = _describeMethod(methodIndex);
-    final String description = pointer.toDartString();
-    _freeRustString(pointer);
-    return description;
+    final ffi.Pointer<ffi.Char> pointer = _describeMethod(methodIndex);
+    try {
+      final ffi.Pointer<pkg_ffi.Utf8> utf8Ptr = pointer.cast<pkg_ffi.Utf8>();
+      return utf8Ptr.toDartString();
+    } finally {
+      _freeRustString(pointer);
+    }
   }
 
   String _methodLabel(int methodIndex) {
@@ -111,31 +140,3 @@ class RustPaymentOutcome {
   final String message;
   final String methodDescription;
 }
-
-/// Rust struct translated to an FFI-compatible Dart representation.
-class FfiPaymentResult extends Struct {
-  @Int32()
-  external int status;
-
-  @Double()
-  external double riskScore;
-
-  external Pointer<Utf8> message;
-}
-
-typedef _ProcessPaymentNative = FfiPaymentResult Function(
-  Double amount,
-  Double tip,
-  Int32 method,
-);
-typedef _ProcessPaymentDart = FfiPaymentResult Function(
-  double amount,
-  double tip,
-  int method,
-);
-
-typedef _FreeStringNative = Void Function(Pointer<Utf8>);
-typedef _FreeStringDart = void Function(Pointer<Utf8>);
-
-typedef _DescribeMethodNative = Pointer<Utf8> Function(Int32 method);
-typedef _DescribeMethodDart = Pointer<Utf8> Function(int method);
